ca99d936f37a829ffe569c31626d0c05c47d3d70
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-21 08:29:17 +0100

    scripts: add feature-nft-spin-tickets component 7

--- scripts/feature-nft-spin-tickets-7.js
+++ scripts/feature-nft-spin-tickets-7.js
@@ -3,7 +3,7 @@
  * Utility for: NFT-based spin ticket system
  */
 
-const SCRIPT_VERSION = '7.0.0';
+const SCRIPT_VERSION = '7.0.0-1776756389';
 
 function validateInput7(params) {
   const required = ['address', 'amount'];
