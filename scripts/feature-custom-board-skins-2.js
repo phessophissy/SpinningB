3d059e681e20b657cd8fd6f2c7a8571081aa7a91
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-21 08:26:56 +0100

    scripts: implement feature-custom-board-skins logic part 2

--- scripts/feature-custom-board-skins-2.js
+++ scripts/feature-custom-board-skins-2.js
@@ -3,7 +3,7 @@
  * Utility for: Custom board skins and themes
  */
 
-const SCRIPT_VERSION = '2.0.0';
+const SCRIPT_VERSION = '2.0.0-1776756389';
 
 function validateInput2(params) {
   const required = ['address', 'amount'];
